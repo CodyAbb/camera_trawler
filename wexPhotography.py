d2ed304fd74ab80f2210e38215c67e87b3eb689f
Cody Abbott <cody.abbott16@gmail.com>
2020-08-25 17:04:45 +0100

    wex function uses full product list page

--- wexPhotography.py
+++ wexPhotography.py
@@ -20,7 +20,7 @@ def wex_photography_cameras(brand):
     def generate_camera_info(cameras):
         for camera in cameras:
             try:
-                model = camera.find_element_by_tag_name('h3').text.split('Used')[1]
+                model = camera.find_element_by_tag_name('h3').text.split('Used ')[1]
                 try:
                     price = camera.find_element_by_class_name('price').text
                 except:
@@ -36,7 +36,7 @@ def wex_photography_cameras(brand):
             except:
                 pass
 
-    driver.get(f'https://www.wexphotovideo.com/used-cameras/#esp_category_cf=Manufacturer&esp_category_filter_Manufacturer={brand_search}')
+    driver.get(f'https://www.wexphotovideo.com/used-cameras/#esp_category_cf=Manufacturer&esp_category_filter_Manufacturer={brand_search}&esp_category_viewall=y')
     brand_cameras = driver.find_elements_by_class_name('listing-product')
     generate_camera_info(brand_cameras)
 
